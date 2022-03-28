67fb2fd50b5a84f8b599a593467565bd84d710e7
VladimirPokorny <pokorny.v@email.cz>
2022-03-28 14:48:36 +0200

    initial commit

---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_template_pca10040_s132/sensorsim.d
@@ -0,0 +1,5 @@
+Output/Release/Obj/ble_app_template_pca10040_s132/sensorsim.o: \
+ C:\NRF52_TEST\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.c \
+ C:\NRF52_TEST\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.70a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.70a/include/stdbool.h
